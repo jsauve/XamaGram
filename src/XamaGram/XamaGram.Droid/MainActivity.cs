40bfa4101f070c8d200671abdec0ae957c256a91
Joe Sauve <joe@joesauve.com>
2015-05-13 17:16:46 -0500

    Updated Android app display name

--- src/XamaGram/XamaGram.Droid/MainActivity.cs
+++ src/XamaGram/XamaGram.Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace XamaGram.Droid
 {
-	[Activity(Label = "FormaGram.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "XamaGram", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate(Bundle bundle)
